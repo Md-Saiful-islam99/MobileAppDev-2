f69cf7b0b1e9c9c626c538076e949d6be4534aa5
saifulislam2711 <saiful111066@gmail.com>
2021-11-29 17:38:48 +0200

    basic BMI calculator

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun calculateBmi(view: View) {
+        val weight = findViewById<EditText>(R.id.weightEditText).text.toString().toFloat()
+        val height = findViewById<EditText>(R.id.heightEditText).text.toString().toFloat();
+
+        val BMI = weight / (height / 100 * height / 100 )
+
+        findViewById<TextView>(R.id.bmiResult).text = BMI.toString();
+
+
+
+    }
+}
